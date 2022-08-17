6d4ce9e8416b02c596b814d9f3215d4f517a2c70
PBillingsby <philbillingsby@gmail.com>
2022-08-17 16:29:02 -0400

    add link to ans-109

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -16,7 +16,7 @@ function HeroHome() {
             <div className="max-w-3xl mx-auto" data-aos="zoom-y-out" data-aos-delay="150">
               <p className="text-left h4 text-gray-800 mb-4 bg-clip-text">Decentralized Verification for Arweave Wallets.</p>
               <p className="text-left text-lg mb-4 text-gray-600 bg-clip-text">VouchDAO allows users to verify once and be trusted everywhere on the Permaweb, while allowing developers to verify them with 2 lines of code.</p>
-              <p className="text-left text-lg mb-4 text-gray-600 bg-clip-text">It all starts with the new Vouch data protocol, defined by ANS-109. ANS-109 allows anyone on the permaweb to "vouch" for the identity & humanity of any other address. Anyone can run a Vouch server, providing attestations on Arweave about user identities.</p>
+              <p className="text-left text-lg mb-4 text-gray-600 bg-clip-text">It all starts with the new Vouch data protocol, defined by <a className="text-[#6495ED]" href="https://github.com/ArweaveTeam/arweave-standards/blob/ans-109/ans/ANS-109.md" target="_blank">ANS-109</a>. ANS-109 allows anyone on the permaweb to "vouch" for the identity & humanity of any other address. Anyone can run a Vouch server, providing attestations on Arweave about user identities.</p>
               <p className="text-left text-lg mb-4 text-gray-600 bg-clip-text">VouchDAO is a decentralized verification layer on top of the Vouch standard. It is a community that votes together on which vouching services are trustworthy.</p>
               <p className="text-left text-lg text-gray-600 bg-clip-text">Community owned and operated. <a className="text-[#6495ED]" href="https://twitter.com/vouchdao" target="_blank">Join us</a>.</p>
             </div>
