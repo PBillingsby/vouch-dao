893284b8021a839c7f0fbdaa3a746a5cfc48ca6f
PBillingsby <philbillingsby@gmail.com>
2022-08-16 22:28:03 -0400

    add responsiveness to code blocks

--- src/partials/Features.jsx
+++ src/partials/Features.jsx
@@ -46,7 +46,7 @@ function Features({ offset }) {
                   <div className="text-gray-600 pt-4">Verify using Vouch Twitter Service, the first trusted node today.</div>
                   <div className="text-gray-600 py-4"><a href="https://vouch-v0.hyper.io" target="_blank" style={{ color: '#6495ED' }}>Get vouched</a> with the only blue tick on the Permaweb!</div>
                 </div>
-                <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
+                <div className="hidden md:flex lg:flex  justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
                   <ShieldLock
                     size={26}
                     strokeWidth={2}
@@ -55,19 +55,19 @@ function Features({ offset }) {
                 </div>
               </div>
               <div
-                className={` flex items-center py-6 text-lg p-5 rounded-md border-2 transition duration-300 ease-in-out mb-3`}
+                className={`  md:flex lg:flex md:items-center lg:items-center lg:py-6 text-lg p-5 rounded-md border-2 transition duration-300 ease-in-out mb-3`}
               >
                 <div>
                   <div className="font-bold text-3xl leading-snug tracking-tight mb-1">For developers</div>
                   <div className="text-gray-600 py-4">Integrating VouchDAO takes 2 minutes and all of pain out of bots on the web.</div>
                   <div className="max-w-sm mx-auto pt-4" data-aos="zoom-y-out" data-aos-delay="300">
-                    <pre className="rounded-md max-w-xs flex"><code lang='javascript' className="bg-[#eee] p-[1rem]">{npmCommand}</code><button className="pl-2" onClick={() => false}><Copy color="#444" onClick={() => handleCopy()} /></button></pre>
+                    <pre className="rounded-md flex"><code lang='javascript' className="bg-[#eee] p-[1rem]">{npmCommand}</code><button className="pl-2" onClick={() => false}><Copy color="#444" onClick={() => handleCopy()} /></button></pre>
                   </div>
                   <div className="max-w-sm mx-auto py-4" data-aos="zoom-y-out" data-aos-delay="300">
                     <pre className="rounded-md text-sm"><code lang='javascript' className="bg-[#eee]">{setupCommand}</code></pre>
                   </div>
                 </div>
-                <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
+                <div className="hidden md:flex lg:flex justify-center items-center w-8 h-8 bg-white rounded-full shadow flex-shrink-0 ml-3">
                   <Tool
                     size={26}
                     strokeWidth={2}
@@ -77,8 +77,8 @@ function Features({ offset }) {
               </div>
             </div>
           </div>
-          <div className="lg:max-w-[45vw]">
-            <div className="mx-auto md:mb-0 border border-gray-300 border-2 rounded-md p-1" data-aos="zoom-y-out fade-left" >
+          <div>
+            <div className="lg:max-w-[45vw] mx-auto md:mb-0 border border-gray-300 border-2 rounded-md p-1" data-aos="zoom-y-out fade-left" >
               <img src={VouchFlow} />
             </div>
           </div>
